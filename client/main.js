12ba9a97a7a908b0fe5e04278626b80fc8dcf33d
srikowsika <srikowsika.guna@aspiresys.com>
2019-10-31 20:20:09 +0530

    changes made

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
+function redirectresult(){
+    window.location.href='/result';
+}
 
+function redirectdetails(){
+    window.location.href='/studentdetails';
+}
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var sql = require('mssql');
@@ -30,15 +30,27 @@ var config = {
 server.listen(PORT, function () {
   console.log('server running');
 });
-
+var env=process.env.ENVCONNECTSTR;
+if(env==undefined)
+{
+var envConnectionString="postgres://kujywnbzxawals:7aba159539b9345d0bb7dc92257aa0e6c1f1a5391f54fc965ae7a2a4d40489fe@ec2-54-83-55-122.compute-1.amazonaws.com:5432/d848jirpgs8ls";
+}
+else{
+var envConnectionString=process.env.DATABASE_URL;
+}
 
 const pool = new Pool({
-  connectionString: "postgres://kujywnbzxawals:7aba159539b9345d0bb7dc92257aa0e6c1f1a5391f54fc965ae7a2a4d40489fe@ec2-54-83-55-122.compute-1.amazonaws.com:5432/d848jirpgs8ls",
-  ssl: true,
+ connectionString: envConnectionString,
+ ssl: true,
 });
 
+// const pool = new Pool({
+//   connectionString: "postgres://kujywnbzxawals:7aba159539b9345d0bb7dc92257aa0e6c1f1a5391f54fc965ae7a2a4d40489fe@ec2-54-83-55-122.compute-1.amazonaws.com:5432/d848jirpgs8ls",
+//   ssl: true,
+// });
 
-app.get('/db', async (req, res) => {
+
+app.get('/studentdetails', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM student_detail');
@@ -54,7 +66,6 @@ app.get('/db', async (req, res) => {
       else {
         for (let row of record) {
           var query = 'Insert into studentlist values(' + "'" + row.id + "'" + ',' + "'" + row.program_name + "'" + ',' + row.year + ',' + row.budget + ',' + row.forcast + ',' + row.actual + ',' + row.created_by + ')';
-          console.log(query);
           var request = new sql.Request();
           request.query(query, function (err, res) {
             if (err) {
@@ -78,30 +89,23 @@ app.get('/db', async (req, res) => {
 });
 
 
-// app.get("/user", function (req, res) {
-//   var query = "INSERT INTO student_list (id, programname, budget, forcast, actual, createdby )VALUES ('1yuyuyu','NEU-MBA',25.00,23.00,13,1)";
-//   sql.close();
-//   sql.connect(config, function (err) {
-//     if (err) {
-//       console.log("Error while connecting database :- " + err);
-//       console.log(err);
-//     }
-//     else {
+app.get('/result', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM student_detail');
+    //var total = result.rows.length;
+   
+    res.send(result);
+  }
 
-//       var request = new sql.Request();
-//       request.query(query, function (err, res) {
-//         if (err) {
-//           console.log("Error while querying database :- " + err);
-//           console.log(err);
-//         }
-//         else {
-//           console.log(res);
-//         }
-//       });
-//     }
-//   });
-//   res.send('Insertion successfull');
-// });
+  catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+
+});
+
+
 
 
 
